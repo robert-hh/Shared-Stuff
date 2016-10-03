f237445e2ede4de7aff71ef6c9ab82009bcb1344
Robert HH <robert@hammelrath.com>
2016-10-03 20:29:28 +0200

    uftpserver: Add date/time values in ls command

--- uftpserver.py
+++ uftpserver.py
@@ -1,13 +1,21 @@
 import socket
 import network
 import os
+from time import localtime
+month_name = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 def send_list_data(cwd, dataclient):
     for file in os.listdir(cwd):
         stat = os.stat(get_absolute_path(cwd, file))
         file_permissions = "drwxr-xr-x" if (stat[0] & 0o170000 == 0o040000) else "-rw-r--r--"
         file_size = stat[6]
-        description = "{}    1 owner group {:>13} Jan 1  1980 {}\r\n".format(file_permissions, file_size, file)
+        tm = localtime(stat[7])
+        if tm[0] != localtime()[0]:
+            description = "{}    1 owner group {:>10} {} {:2} {:>5} {}\r\n".format(
+                file_permissions, file_size, month_name[tm[1]], tm[2], tm[0], file)
+        else:
+            description = "{}    1 owner group {:>10} {} {:2} {:02}:{:02} {}\r\n".format(
+                file_permissions, file_size, month_name[tm[1]], tm[2], tm[3], tm[4], file)
         dataclient.sendall(description)
         
 def send_file_data(path, dataclient):
@@ -65,7 +73,7 @@ def ftpserver():
                     data = cl.readline().decode("utf-8").replace("\r\n", "")
                     if len(data) <= 0:
                         print("Client is dead")
-                        trying = False
+                        # trying = False
                         break
                     
                     command, payload =  (data.split(" ") + [""])[:2]
@@ -77,8 +85,8 @@ def ftpserver():
                         cl.sendall("230 Logged in.\r\n")
                     elif command == "SYST":
                         cl.sendall("215 ESP8266 MicroPython\r\n")
-                    elif command == "SYST":
-                        cl.sendall("502\r\n")
+#                    elif command == "SYST":
+#                        cl.sendall("502\r\n")
                     elif command == "PWD":
                         cl.sendall('257 "{}"\r\n'.format(cwd))
                     elif command == "CWD":
