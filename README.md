cbe3113af22cdb91bfa72ba8e5d8f1d0c8af2c31
Robert HH <robert@hammelrath.com>
2016-10-08 14:55:59 +0200

    all: Update
    README.md: add note about uftpserver.py
    firmware-combined.bin: Oct 8 build, with uftpserver and upysh embedded
    mcuimg.bin: Oct 8 build
    uftpserver.py: Just the recent state of the script

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Shared-Stuff
-mcuimg.bin: build for wipy w/o threading, but support of .mpy files
-esp8266-firmware-xxxx.bin: build for ESP8266 with support of .mpy files
-mpy-cross.exe: Windows version of Micropython cross-compiler
-
+- mcuimg.bin: build for wipy w/o threading, but support of .mpy files
+- firmware-combined.bin: build for ESP8266 with support of .mpy files,
+embedded uftpserver.py and upysh.py
+- mpy-cross.exe: Windows version of Micropython cross-compiler
+- uftpserver.py: chrisgp's uftpserver with some extensions
--- uftpserver.py
+++ uftpserver.py
@@ -1,25 +1,30 @@
 import socket
 import network
-import os
+import uos
+import gc
 from time import localtime
+
 month_name = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-def send_list_data(cwd, dataclient):
-    for file in os.listdir(cwd):
-        stat = os.stat(get_absolute_path(cwd, file))
-        file_permissions = "drwxr-xr-x" if (stat[0] & 0o170000 == 0o040000) else "-rw-r--r--"
-        file_size = stat[6]
-        tm = localtime(stat[7])
-        if tm[0] != localtime()[0]:
-            description = "{}    1 owner group {:>10} {} {:2} {:>5} {}\r\n".format(
-                file_permissions, file_size, month_name[tm[1]], tm[2], tm[0], file)
+def send_list_data(path, dataclient, full):
+    for fname in uos.listdir(path):
+        if full:
+            stat = uos.stat(get_absolute_path(path,fname))
+            file_permissions = "drwxr-xr-x" if (stat[0] & 0o170000 == 0o040000) else "-rw-r--r--"
+            file_size = stat[6]
+            tm = localtime(stat[7])
+            if tm[0] != localtime()[0]:
+                description = "{}    1 owner group {:>10} {} {:2} {:>5} {}\r\n".format(
+                    file_permissions, file_size, month_name[tm[1]], tm[2], tm[0], fname)
+            else:
+                description = "{}    1 owner group {:>10} {} {:2} {:02}:{:02} {}\r\n".format(
+                    file_permissions, file_size, month_name[tm[1]], tm[2], tm[3], tm[4], fname)
         else:
-            description = "{}    1 owner group {:>10} {} {:2} {:02}:{:02} {}\r\n".format(
-                file_permissions, file_size, month_name[tm[1]], tm[2], tm[3], tm[4], file)
+            description = fname + "\r\n"
         dataclient.sendall(description)
         
 def send_file_data(path, dataclient):
-    with open(path) as file:
+    with open(path, "r") as file:
         chunk = file.read(128)
         while len(chunk) > 0:
             dataclient.sendall(chunk)
@@ -33,13 +38,24 @@ def save_file_data(path, dataclient):
             chunk = dataclient.read(128)
 
 def get_absolute_path(cwd, payload):
-    # if it doesn't start with / consider
-    # it a relative path
-    if not payload.startswith("/"):
-        payload = cwd + "/" + payload
-    # and don't leave any trailing /
-    return payload.rstrip("/")
-
+    # Just a few special cases "..", "." and ""
+    # If payload start's with /, set cwd to / 
+    # and consider the remainder a relative path
+    if payload.startswith('/'):
+        cwd = "/"
+    for token in payload.split("/"):
+        if token == '..':
+            if cwd != '/':
+                cwd = '/'.join(cwd.split('/')[:-1])
+                if cwd == '': 
+                    cwd = '/'
+        elif token != '.' and token != '':
+            if cwd == '/':
+                cwd += token
+            else:
+                cwd = cwd + '/' + token
+    return cwd
+    
 def ftpserver():
 
     DATA_PORT = 13333
@@ -57,29 +73,31 @@ def ftpserver():
     datasocket.listen(1)
     datasocket.settimeout(10)
 
-    dataclient = None
-
+    msg_250_OK = '250 OK\r\n'
+    msg_550_fail = '550 Failed\r\n'
     try:
         dataclient = None
-        fromname = ""
-        toname = ""
+        fromname = None
         while True:
-            cwd = "/"
             cl, remote_addr = ftpsocket.accept()
             cl.settimeout(300)
+            cwd = '/'
             try:
-                print("FTP connection from:", remote_addr)
+                # print("FTP connection from:", remote_addr)
                 cl.sendall("220 Hello, this is the ESP8266.\r\n")
                 while True:
-                    data = cl.readline().decode("utf-8").replace("\r\n", "")
+                    gc.collect()
+                    data = cl.readline().decode("utf-8").rstrip("\r\n")
                     if len(data) <= 0:
-                        print("Client is dead")
+                        print("Client disappeared")
                         break
                     
-                    command, payload =  (data.split(" ") + [""])[:2]
-                    command = command.upper()
+                    command = data.split(" ")[0].upper()
+                    payload = data[len(command):].lstrip()
+
+                    path = get_absolute_path(cwd, payload)
                     
-                    print("Command={}, Payload={}".format(command, payload))
+                    print("Command={}, Payload={}, Path={}".format(command, payload, path))
                     
                     if command == "USER":
                         cl.sendall("230 Logged in.\r\n")
@@ -92,97 +110,112 @@ def ftpserver():
                     elif command == "PWD":
                         cl.sendall('257 "{}"\r\n'.format(cwd))
                     elif command == "CWD":
-                        path = get_absolute_path(cwd, payload)
                         try:
-                            files = os.listdir(path)
+                            files = uos.listdir(path)
                             cwd = path
-                            cl.sendall('250 Directory changed successfully\r\n')
+                            cl.sendall(msg_250_OK)
                         except:
-                            cl.sendall('550 Failed to change directory\r\n')
-                    elif command == "EPSV":
-                        cl.sendall('502\r\n')
+                            cl.sendall(msg_550_fail)
+                    elif command == "CDUP":
+                        cwd = get_absolute_path(cwd, "..")
+                        cl.sendall(msg_250_OK)
                     elif command == "TYPE":
                         # probably should switch between binary and not
                         cl.sendall('200 Transfer mode set\r\n')
                     elif command == "SIZE":
-                        path = get_absolute_path(cwd, payload)
                         try:
-                            size = os.stat(path)[6]
+                            size = uos.stat(path)[6]
                             cl.sendall('213 {}\r\n'.format(size))
                         except:
-                            cl.sendall('550 Could not get file size\r\n')
+                            cl.sendall(msg_550_fail)
                     elif command == "QUIT":
                         cl.sendall('221 Bye.\r\n')
+                        break
                     elif command == "PASV":
                         addr = network.WLAN().ifconfig()[0]
-                        cl.sendall('227 Entering Passive Mode ({},{},{}).\r\n'.format(addr.replace('.',','), DATA_PORT>>8, DATA_PORT%256))
+                        cl.sendall('227 Entering Passive Mode ({},{},{}).\r\n'.format(
+                            addr.replace('.',','), DATA_PORT>>8, DATA_PORT%256))
                         dataclient, data_addr = datasocket.accept()
-                        print("FTP Data connection from:", data_addr)
-                    elif command == "LIST":
+                        # print("FTP Data connection from:", data_addr)
+                    elif command == "LIST" or command == "NLST":
+                        if not payload.startswith("-"):
+                            place = path
+                        else: 
+                            place = cwd
                         try:
-                            send_list_data(cwd, dataclient)
-                            dataclient.close()
+                            send_list_data(place, dataclient, command == "LIST" or payload == "-l")
                             cl.sendall("150 Here comes the directory listing.\r\n")
                             cl.sendall("226 Listed.\r\n")
                         except:
-                            cl.sendall('550 Failed to list directory\r\n')
-                        finally:
+                            cl.sendall(msg_550_fail)
+                        if dataclient is not None:
                             dataclient.close()
+                            dataclient = None
                     elif command == "RETR":
                         try:
-                            send_file_data(get_absolute_path(cwd, payload), dataclient)
-                            dataclient.close()
+                            send_file_data(path, dataclient)
                             cl.sendall("150 Opening data connection.\r\n")
                             cl.sendall("226 Transfer complete.\r\n")
                         except:
-                            cl.sendall('550 Failed to send file\r\n')
-                        finally:
+                            cl.sendall(msg_550_fail)
+                        if dataclient is not None:
                             dataclient.close()
+                            dataclient = None
                     elif command == "STOR":
                         try:
                             cl.sendall("150 Ok to send data.\r\n")
-                            save_file_data(get_absolute_path(cwd, payload), dataclient)
-                            dataclient.close()
+                            save_file_data(path, dataclient)
                             cl.sendall("226 Transfer complete.\r\n")
                         except:
-                            cl.sendall('550 Failed to send file\r\n')
-                        finally:
+                            cl.sendall(msg_550_fail)
+                        if dataclient is not None:
                             dataclient.close()
-                    elif command == "DELE" or command == "RMD":
+                            dataclient = None
+                    elif command == "DELE":
                         try:
-                            os.remove(get_absolute_path(cwd, payload))
-                            cl.sendall("250 Object deleted.\r\n")
+                            uos.remove(path)
+                            cl.sendall(msg_250_OK)
                         except:
-                            cl.sendall('550 Failed to delete\r\n')
+                            cl.sendall(msg_550_fail)
+                    elif command == "RMD":
+                        try:
+                            uos.rmdir(path)
+                            cl.sendall(msg_250_OK)
+                        except:
+                            cl.sendall(msg_550_fail)
                     elif command == "MKD":
                         try:
-                            os.mkdir(payload)
-                            cl.sendall("250 Directory created.\r\n")
+                            uos.mkdir(path)
+                            cl.sendall(msg_250_OK)
                         except:
-                            cl.sendall('550 Failed to create\r\n')
+                            cl.sendall(msg_550_fail)
                     elif command == "RNFR":
-                            fromname = get_absolute_path(cwd, payload)
+                            fromname = path
                             cl.sendall("350 Rename from\r\n")
                     elif command == "RNTO":
-                            if (fromname): 
+                            if fromname is not None: 
                                 try:
-                                    os.rename(fromname, get_absolute_path(cwd, payload))
-                                    cl.sendall("250 File renamed.\r\n")
+                                    uos.rename(fromname, path)
+                                    cl.sendall(msg_250_OK)
                                 except:
-                                    cl.sendall('550 Failed to rename\r\n')
+                                    cl.sendall(msg_550_fail)
                             else:
-                                cl.sendall('550 Failed to rename\r\n')
-                            fromname = ""
+                                cl.sendall(msg_550_fail)
+                            fromname = None
                     else:
                         cl.sendall("502 Unsupported command.\r\n")
-                        print("Unsupported command {} with payload {}".format(command, payload))
-                        
-            finally:
+                        # print("Unsupported command {} with payload {}".format(command, payload))
+            except Exception as err:
+                print(err)  
+
+            finally:          
                 cl.close()
+                cl = None
     finally:
         datasocket.close()
         ftpsocket.close()
         if dataclient is not None:
             dataclient.close()
-            
+
+           
 ftpserver()
